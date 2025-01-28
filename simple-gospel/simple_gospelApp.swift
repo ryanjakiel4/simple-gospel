ec8e4dddde58364bb7bb5eb7431cdc7328ac0fbf
ryanjakiel4 <ryanjakiel4@gmail.com>
2025-01-28 16:40:35 -0600

    Initial Commit

---
+++ simple-gospel/simple_gospelApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_gospelApp.swift
+//  simple-gospel
+//
+//  Created by Ryan Jakiel on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct simple_gospelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ simple-gospelTests/simple_gospelTests.swift
@@ -0,0 +1,17 @@
+//
+//  simple_gospelTests.swift
+//  simple-gospelTests
+//
+//  Created by Ryan Jakiel on 1/28/25.
+//
+
+import Testing
+@testable import simple_gospel
+
+struct simple_gospelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
